096d4c526eeb70da64c1c73ef49ba6a6a891316b
sahilkrishna123 <sahilkrishna1243@gmail.com>
2026-03-09 21:50:36 +0500

    Lazy Initialization

---
+++ src/main/java/App05LazyInitializationExample/AppLauncherLI.java
@@ -0,0 +1,42 @@
+package App05LazyInitializationExample;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+class ClassA{
+
+}
+
+@Component
+@Lazy
+class ClassB{
+    private ClassA classA;
+
+    public ClassB(ClassA classA){
+        // Logic
+        System.out.println("Some Initialization Logic");
+        this.classA = classA;
+    }
+    public void doSomething(){
+        System.out.println("Do something");
+    }
+}
+
+@Configuration
+@ComponentScan
+public class AppLauncherLI {
+    public static void main(String[] args){
+        try(var context = new AnnotationConfigApplicationContext(AppLauncherLI.class)){
+//            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+            System.out.println("Context initialization has completed");
+
+            context.getBean(ClassB.class).doSomething();
+        }
+    }
+}
