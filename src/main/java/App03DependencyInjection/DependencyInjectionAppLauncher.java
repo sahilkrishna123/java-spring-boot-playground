60e42522b641b1c4ed68b3d093c33ccb8b28505a
sahilkrishna123 <sahilkrishna1243@gmail.com>
2026-03-09 20:33:33 +0500

    DI: Setter Injection

--- src/main/java/App03DependencyInjection/DependencyInjectionAppLauncher.java
+++ src/main/java/App03DependencyInjection/DependencyInjectionAppLauncher.java
@@ -11,11 +11,23 @@ import java.util.Arrays;
 @Component
 class BusinessClass{
     // Field Injection Using Autowired Keywords
-    @Autowired
+//    @Autowired
     Dependency1 dependency1;
 
-    @Autowired
+//    @Autowired
     Dependency2 dependency2;
+
+    // Setter Injection
+    @Autowired
+    public void setDependency1(Dependency1 dependency1){
+        System.out.println("Setter Injection: setDependency1");
+        this.dependency1 = dependency1;
+    }
+    @Autowired
+    public void setDependency2(Dependency2 dependency2){
+        System.out.println("Setter Injection: setDependency1");
+        this.dependency2 = dependency2;
+    }
 
     public String toString(){
         return "Using " + dependency1 + " and " + dependency2;
