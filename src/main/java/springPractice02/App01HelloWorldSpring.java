e0c48732a94a160d2071ebffd9b02ab5db3cbfc6
sahilkrishna123 <sahilkrishna1243@gmail.com>
2026-03-09 14:33:47 +0500

    Create Beans, Reuse existing Beans with two approaches

---
+++ src/main/java/springPractice02/App01HelloWorldSpring.java
@@ -0,0 +1,30 @@
+package springPractice02;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App01HelloWorldSpring {
+    public static void main (String[] args){
+
+        // 1: Launch spring context (launch configuration class)
+        var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+
+
+        // 2: Configuration Class
+        // Add @Configuration to that class and add @Bean to the functions.
+
+        // 3: Retrieve the bean
+
+        System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
+
+        System.out.println(context.getBean("person"));
+
+        System.out.println(context.getBean("person3Parameter"));
+
+        System.out.println(context.getBean("address2"));
+
+        // You can extract bean by specifying the type
+        //         System.out.println(context.getBean(Address.class));
+
+    }
+}
---
+++ src/main/java/springPractice02/HelloWorldConfiguration.java
@@ -0,0 +1,37 @@
+package springPractice02;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+record Person (String name, int age, Address address){ };
+
+record Address (String firstLine, String city){ };
+@Configuration
+public class HelloWorldConfiguration {
+
+    @Bean
+    public String name(){
+        return "Sahil";
+    }
+
+    @Bean
+    public int age(){
+        return 22;
+    }
+
+    @Bean
+    public Person person(){
+        return new Person("Moin", 21, new Address("Main Street", "London"));
+    }
+
+    @Bean
+    public Person person3Parameter(String name, int age, Address address2){
+        return new Person(name, age, address2);
+        // name, age and address2 are the names of beans.
+    }
+
+    @Bean(name = "address2")
+    public Address address(){
+        return new Address("Wall Street", "Los Angles");
+    }
+}
