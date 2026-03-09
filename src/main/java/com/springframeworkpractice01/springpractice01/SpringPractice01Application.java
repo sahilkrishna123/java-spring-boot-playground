e38b59efc0c43f5ec4cf90cf4fec9b3283cb5db6
sahilkrishna123 <sahilkrishna1243@gmail.com>
2026-03-09 13:05:21 +0500

    Initial Commit

---
+++ src/main/java/com/springframeworkpractice01/springpractice01/SpringPractice01Application.java
@@ -0,0 +1,15 @@
+package com.springframeworkpractice01.springpractice01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPractice01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPractice01Application.class, args);
+
+        System.out.println("hello sahil");
+    }
+
+}
---
+++ src/test/java/com/springframeworkpractice01/springpractice01/SpringPractice01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframeworkpractice01.springpractice01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPractice01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
